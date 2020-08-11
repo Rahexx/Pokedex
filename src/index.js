1c8fdc292f03d16f134df029e8b60d7d732f8b00
Rahexx <endriu1297@gmail.com>
2020-08-11 19:14:35 +0200

    download data random pokemon. Show random pokemon  name and image on screen

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ const button = document.querySelector('.pokemonRandom__randomButton');
 let open = false;
 
 function fallPokeball() {
+  const main = document.querySelector('.pokemonRandom');
+  const mainWidth = main.offsetWidth;
+
+  const jumpHeight =
+    mainWidth > 500 && mainWidth < 1024 && mainWidth != 768 ? '75%' : '25vh';
+
   let tl = gsap.timeline();
   tl.to('.pokemonRandom__randomButton', { scale: 1.2, duration: 0.5 })
     .to('.pokemonRandom__randomButton', { scale: 0, duration: 1 })
     .to('.pokemonRandom__pokeball', {
       scale: 0.5,
-      y: '25vh',
+      y: jumpHeight,
       ease: 'bounce.out',
       duration: 2,
     })
@@ -55,26 +61,35 @@ function animateArrow() {
     'animation-name: moveDown; animation-duration: 2s; animation-iteration-count: infinite;';
 }
 
+function createImagePokemon(srcImg) {
+  const img = document.createElement('img');
+
+  img.src = srcImg;
+  img.classList.add('pokemonContainer__pokemon');
+  img.style.cssText = 'transform: scale(0)';
+
+  return img;
+}
+
+function createNamePokemon(name) {
+  const h1 = document.createElement('h1');
+
+  h1.classList.add('pokemonContainer__name');
+  h1.style.cssText = 'transform: scale(0);';
+  h1.textContent = name;
+
+  return h1;
+}
+
 function addPokemon(srcImg, name) {
   const main = document.querySelector('.pokemonRandom');
   const fragment = document.createDocumentFragment();
   const div = document.createElement('div');
-  const img = document.createElement('img');
-  const h1 = document.createElement('h1');
 
   div.classList.add('pokemonContainer');
-  img.src = srcImg;
 
-  img.classList.add('pokemonContainer__pokemon');
-  h1.classList.add('pokemonContainer__name');
-
-  img.style.cssText = 'transform: scale(0)';
-  h1.style.cssText = 'transform: scale(0);';
-
-  h1.textContent = name;
-
-  div.appendChild(h1);
-  div.appendChild(img);
+  div.appendChild(createNamePokemon(name));
+  div.appendChild(createImagePokemon(srcImg));
 
   fragment.appendChild(div);
 
